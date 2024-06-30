bb90182f5b357c5f2addc3236e7aba5dd42d4b87
JosephGrimshaw <josephgrimshaw1@gmail.com>
2024-06-30 13:29:56 +0100

    tutorialPart6

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Button, FormLabel } from "@mui/material";
+import { Grid } from "@mui/material";
+import { Typography } from '@mui/material';
+import { TextField } from '@mui/material';
+import { FormHelperText } from '@mui/material';
+import { FormControl } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Radio } from '@mui/material';
+import { RadioGroup } from '@mui/material';
+import { FormControlLabel } from '@mui/material';
 
 export default class HomePage extends Component {
     constructor(props) {
@@ -12,7 +22,28 @@ export default class HomePage extends Component {
         return (
             <Router>
                 <Routes>
-                    <Route path='/' element={<p>Hiiii</p>} />
+                    <Route path='/' element={
+                        <Grid container spacing="1">
+                            <Grid item xs={12} align="center">
+                                <Typography>Welcome!</Typography>
+                            </Grid>
+                            <Grid item xs={12} align="center">
+                                <FormControl component="fieldset">
+                                    <FormHelperText>
+                                        <div align="center">
+                                        Get started by creating or joining a room!
+                                        </div>
+                                    </FormHelperText>
+                                </FormControl>
+                            </Grid>
+                                <Grid item xs={12} align="center">
+                                    <Button color="primary" variant="contained" to="/create" component={Link}>Create a Room</Button>
+                                </Grid>
+                                <Grid item xs={12} align="center">
+                                    <Button color="primary" variant="contained" to="/join" component={Link}>Join a Room</Button>
+                            </Grid>
+                        </Grid>
+                    } />
                     <Route path='/join' element={<RoomJoinPage />} />
                     <Route path='/create' element={<CreateRoomPage />} />
                 </Routes>
--- music_controller/frontend/src/components/RoomJoinPage.js
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { Button, FormLabel } from "@mui/material";
+import { Grid } from "@mui/material";
+import { Typography } from '@mui/material';
+import { TextField } from '@mui/material';
+import { FormHelperText } from '@mui/material';
+import { FormControl } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { Radio } from '@mui/material';
+import { RadioGroup } from '@mui/material';
+import { FormControlLabel } from '@mui/material';
 
 export default class RoomJoinPage extends Component {
+    defaultVotes = 2
     constructor(props) {
         super(props);
     }
 
     render () {
-        return <p>Room Join Page</p>
+        return (
+        /*<Grid container spacing="1">
+            <Grid item xs={12} align="center">
+                <Typography component='h4' variant='h4'>
+                    Create A Room
+                </Typography>
+            </Grid>
+        </Grid>*/
+        <p>Hi</p>
+        );
     }
 }
